986c5d7b620a245813a205f13b8b6d9fb6635cf6
Radhika Mattoo <rm3485@nyu.edu>
2016-02-11 11:49:58 -0500

    finished flattening exercise

--- exercises-ch05.js
+++ exercises-ch05.js
@@ -1,1 +1,9 @@
-// exercises-ch05.js
+// Flattening
+//use reduce and concat to 'flatten' a nested array into a single one that has all the original elements
+var array = [[2, 4, 6], [8], [10, 12]];
+
+var flattenedArray = array.reduce(function(e1, e2){
+  return e1.concat(e2);
+}, []);
+
+console.log(flattenedArray);
